f82307e19238adc8a35bdfac55df4f2598549fe7
Jorge Chavez <jorge.chavez@points.com>
2020-09-27 02:28:57 -0700

    Remove ECMA6

--- app/assets/javascripts/logs.js
+++ app/assets/javascripts/logs.js
@@ -1,19 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(document).ready(() => {
+$(document).ready(function() {
   const cleanSelectedColors = function() {
-    const colorTags = $('.color-select__color');
+    var colorTags = $('.color-select__color');
     for (let i = 0; i < colorTags.length; i++) {
-      const tag = colorTags[i];
+      var tag = colorTags[i];
       $(tag).removeClass('selected');
     }
   }
   $('.color-select__color').on('click', function() {
     cleanSelectedColors()
     $(this).addClass('selected');
-    const color = $(this).data('color');
-    console.log(color);
+    var color = $(this).data('color');
     $("#color-input").attr("value", color);
   });
 });
