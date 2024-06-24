68c1a073d63952a401e66c9570f6029e4ed83067
Jorge Chavez <jorgechavzns@gmail.com>
2024-06-24 00:05:45 -0700

    testing

--- app/models/board.rb
+++ app/models/board.rb
@@ -3,7 +3,7 @@ class Board < ApplicationRecord
 
   belongs_to :user
   has_many :logs, dependent: :delete_all
-  has_many :user_boards
+  has_many :user_boards, dependent: :delete_all
 
   def owner?(current_user)
     current_user.id == self.user_id
