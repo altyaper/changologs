d7bed2871d56431694c42294fe819adc2a3fb79a
Jorge Chavez <jorgechavzns@gmail.com>
2024-07-07 21:55:53 -0700

    Revert "Revert "emails again""
    This reverts commit 6838b182c28ad13efc5e19d57d814ab4350e4599.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,17 +34,13 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   # Delivery method
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :mailgun
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.mailgun.org",
-    port: 587,
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["SMTP_USERNAME"],
-    password: ENV["SMTP_PASSWORD"]
+  config.action_mailer.mailgun_settings = {
+    api_key: ENV['CHANGOLOGS_MAILGUN_API_KEY'],
+    domain: ENV['CHANGOLOGS_MAILGUN_DOMAIN']
   }
-
+  
   # Devise
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
